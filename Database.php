58fb4888d673d1fc54208cdeeb00f369ac274f29
M1chal3k28 <balcerakm0@gmail.com>
2024-05-27 19:21:56 +0200

    Database now create automatically when there is an error with connection

--- Database.php
+++ Database.php
@@ -6,10 +6,43 @@
     $dbname = "chat";
 
     // conn object
-    $conn = mysqli_connect($servername, $username, $pass, $dbname);
+    $conn = mysqli_connect($servername, $username, $pass);
 
-    if($conn === false) {
-        echo "<p>Connection failed !</p>";
-        die;
+    $res = $conn->execute_query("SELECT SCHEMA_NAME
+                                 FROM INFORMATION_SCHEMA.SCHEMATA
+                                 WHERE SCHEMA_NAME = 'chat'");
+
+    if($res->num_rows == 0) {
+        $sql = "CREATE DATABASE $dbname;";
+        $usersTab = "CREATE TABLE users(
+            id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+            username VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL
+        );";
+
+        $messagesTab = "CREATE TABLE messages(
+            id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+            sender VARCHAR(255) NOT NULL,
+            receiver VARCHAR(255) NOT NULL,
+            message TEXT NOT NULL
+        );";
+
+        $res = $conn->query($sql);
+        if($res === false) {
+            echo "Failed to create database !";
+            die;
+        }
+        
+        $conn->select_db($dbname);
+
+        $res |= $conn->query($usersTab);
+        $res |= $conn->query($messagesTab);
+
+        if($res === false) {
+            echo "Failed to create database !";
+            die;
+        }
+    } else {
+        $conn->select_db($dbname);
     }
 ?>
